7bfbacb408ca629ad3037448f082ee9e41f73e4b
Alam-Bangi <bangialam12@gmail.com>
2026-09-12 14:24:09 +0530

    Practised Spring Annotations

---
+++ learning/src/main/java/com/learning/game/GamingAppLauncher.java
@@ -0,0 +1,23 @@
+package com.learning.game;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.learning.game")
+public class GamingAppLauncher {
+	
+	public static void main(String[] args) {
+
+		try (var context = 
+				new AnnotationConfigApplicationContext
+					(GamingAppLauncher.class)) {
+
+			context.getBean(GamingConsole.class).up();
+			
+			context.getBean(GameRunner.class).run();
+
+		}
+	}
+}
